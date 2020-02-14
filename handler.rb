fdd33e82fe7606902a0fab8f8e2f311be346c9ac
Francis Borbe <borbefg@gmail.com>
2020-02-14 16:31:00 +0800

    add oauth callback handler

--- handler.rb
+++ handler.rb
@@ -26,6 +26,29 @@ def auth(event:, context:)
   { status: 200, body: credentials }
 end
 
+def callback_handler(event:, context:)
+  user_id     = 'default'
+  oob_uri     = 'urn:ietf:wg:oauth:2.0:oob'
+  scope       = Google::Apis::CalendarV3::AUTH_CALENDAR_EVENTS
+  client_id   = Google::Auth::ClientId.new(ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'])
+  token_store = Google::Auth::Stores::RedisTokenStore.new(redis: Redis.new)
+  authorizer  = Google::Auth::UserAuthorizer.new(client_id, scope, token_store)
+  credentials = authorizer.get_credentials(user_id)
+
+  puts "Getting access tokens..."
+  code = event['queryStringParameters']['code']
+  credentials = authorizer.get_and_store_credentials_from_code(
+    user_id: user_id, code: code, base_url: oob_uri
+  )
+
+  {
+    statusCode: 303,
+    headers: {
+      location: "https://#{event['requestContext']['domainName']}/dev/authorize"
+    }
+  }
+end
+
 def createEvent(event:, context:)
   calendar = Google::Apis::CalendarV3::CalendarService.new
   calendar.authorization = authorize
